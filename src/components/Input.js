1e009d400bae1b60d09fe890997d47e06cc42231
Shane Slone <slone.shane@gmail.com>
2020-10-05 19:43:19 -0400

    few changes to pass test

--- src/components/Input.js
+++ src/components/Input.js
@@ -57,14 +57,13 @@ export default function Input() {
     fontSize: '1.5em',
     marginBottom: '0.3em',
     color: inputValue.length > 10 ? 'crimson' : 'royalblue' /* STEP 2 */,
-    textTransform: 'uppercase',
   };
 
   return (
     <div className='widget-input container'>
       <h2>Input</h2>
       <div id='output' style={style}>
-        {inputValue}
+        {inputValue.toUpperCase()}
       </div>{' '}
       {/* STEP 3 */}
       <div>
